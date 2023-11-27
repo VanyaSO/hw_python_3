8351dd45f97811e0b11912b91e278c0be73e8155
Ivan Ushachov <usacovg@gmail.com>
2023-11-27 21:15:26 +0200

    text

---
+++ task1.py
@@ -0,0 +1,8 @@
+number1 = int(input('Enter number1: '))
+number2 = float(input('Enter number2: '))
+number3 = float(input('Enter number3: '))
+
+print('Sum =', number1 + number2 + number3);
+print('Div =', number1 * number2 * number3);
+
+# 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
