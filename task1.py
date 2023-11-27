b64a247dc310f3c5c0b1b69562cbd7c410a29278
Ivan Ushachov <ushachovg324@gmail.com>
2023-11-27 22:14:47 +0200

    first task

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
+# Користувач вводить із клавіатури три числа. 
+# Необхідно знайти суму чисел, добуток чисел. Результат обчислень вивести на екран.
+
 number1 = int(input('Enter number1: '))
 number2 = float(input('Enter number2: '))
 number3 = float(input('Enter number3: '))
